098193018059aad04ba19441391bdd9413584c4a
san_wyd <kurniawansandy808@gmail.com>
2025-04-16 10:39:26 +0700

    commit transaksi

---
+++ CM1_ASD25/src/nasabah.java
@@ -0,0 +1,11 @@
+public class nasabah {
+    public String norek, nama, namaIbu, nohp, email;
+
+    public nasabah(String norek, String nama, String namaIbu, String nohp, String email) {
+        this.norek = norek;
+        this.nama = nama;
+        this.namaIbu = namaIbu;
+        this.nohp = nohp;
+        this.email = email;
+    }
+}
--- CM1_ASD25/src/transaksi.java
+++ CM1_ASD25/src/transaksi.java
@@ -1,27 +1,14 @@
 public class transaksi {
-    public class Transaksi {
-        String kodeTransaksi;
-        double saldo;
-        double inOutSaldo;
-        double finalSaldo;
-        String tanggalTransaksi;
-        String type;
-        bank bankAcc;
-    
-        public Transaksi(String kodeTransaksi, double saldo, double inOutSaldo, double finalSaldo, 
-                         String tanggalTransaksi, String type, bank bankAcc) {
-            this.kodeTransaksi = kodeTransaksi;
-            this.saldo = saldo;
-            this.inOutSaldo = inOutSaldo;
-            this.finalSaldo = finalSaldo;
-            this.tanggalTransaksi = tanggalTransaksi;
-            this.type = type;
-            this.bankAcc = bankAcc;
-        }
-    
-        public void tampilDataTransaksi() {
-            System.out.printf("%-10s %-15s %-10.2f %-10.2f %-10.2f %-15s %-10s\n",
-                kodeTransaksi, bankAcc.noRekening, saldo, inOutSaldo, finalSaldo, tanggalTransaksi, type);
-        }
+    public String kode, norek, tanggal, tipe;
+    public double saldoAwal, nilaiTransaksi, saldoAkhir;
+
+    public transaksi(String kode, String norek, double saldoAwal, double nilaiTransaksi, double saldoAkhir, String tanggal, String tipe) {
+        this.kode = kode;
+        this.norek = norek;
+        this.saldoAwal = saldoAwal;
+        this.nilaiTransaksi = nilaiTransaksi;
+        this.saldoAkhir = saldoAkhir;
+        this.tanggal = tanggal;
+        this.tipe = tipe;
     }
 }
